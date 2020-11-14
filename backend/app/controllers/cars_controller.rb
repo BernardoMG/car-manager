9702d263fd71fe259b8bdd11d0eacf4591f121d9
BernardoMG <bernardo.marquesg@gmail.com>
2020-11-14 20:06:26 +0000

    Improve docs and added get action for the next endpoint

--- backend/app/controllers/cars_controller.rb
+++ backend/app/controllers/cars_controller.rb
@@ -1,37 +1,63 @@
 class CarsController < ApplicationController
-  # GET /cars
-  # def index
-  #   @cars = Car.all
+  # the param_group :car is defined here to describe the request body.
+  def_param_group :car do
+    param :car, Hash, required: true, desc: 'Car params' do
+      param :available_from, String, required: true, desc: 'Available from, when the car is available for booking.
+                                                            Use the following date format YYYY-mm-dd (e.g 2020/11/14).'
+      param :year, :number, required: true, desc: 'The year of the car.'
+      param :monthly, :decimal, required: true, desc: 'Car subscription price. Decimal value.'
+      param :model_id, :number, required: true, desc: 'Car model id. Model must exist.'
+      param :maker_id, :number, required: true, desc: 'Car maker id. Maker must exist.'
+      param :color_id, :number, required: true, desc: 'Car color id. Color must exist.'
+    end
+  end
 
-  #   render json: @cars
-  # end
+  # the param_group :car_response is defined here to describe the output returned.
+  def_param_group :car_response do
+    property :available_from, String, required: true, desc: 'Available from, when the car is available for booking.'
+    property :year, :number, required: true, desc: 'The year of the car.'
+    property :monthly, :decimal, required: true, desc: 'Car subscription price.'
+    property :model_id, :number, required: true, desc: 'Car model id.'
+    property :maker_id, :number, required: true, desc: 'Car maker id.'
+    property :color_id, :number, required: true, desc: 'Car color id.'
+    property :created_at, String, required: true, desc: 'When the car object was created.'
+    property :updated_at, String, required: true, desc: 'When the car object was updated.'
+  end
+
+  # the param_group :car_generic_errors is defined here to describe the generic response errors.
+  def_param_group :car_generic_errors do
+    error code: 400, desc: 'Bad Request.', meta: 'Review request endpoint.'
+    error code: 408, desc: 'Request Timeout.', meta: 'Contact support team.'
+    error code: 422, desc: 'Unprocessable Entity.', meta: 'Review request parameters.'
+    error code: 500, desc: 'Internal Server Error.', meta: 'Contact support team.'
+  end
+
+  # GET /cars
+  api :GET, '/cars', 'Endpoint to get all cars.
+                      By default, sorted by monthly from lowest to highest
+                      and with an available_from bigger than three months in the future.'
+  formats ['json']
+  # param_group :car
+  param_group :car_generic_errors
+  returns code: 200, desc: 'Request went well.' do
+    property :cars, Array, desc: 'List of all Cars' do
+      param_group :car_response
+    end
+  end
+  def index
+    # @cars = Car.all
+
+    # render json: @cars
+  end
 
   # POST /cars
   api :POST, '/cars', 'Endpoint to create a car.'
   formats ['json']
-  param :car, Hash, required: true, desc: 'Car params' do
-    param :available_from, String, required: true, desc: 'Available from, when the car is available for booking.
-                                                          Use the following date format YYYY-mm-dd (e.g 2020/11/14).'
-    param :year, :number, required: true, desc: 'The year of the car.'
-    param :monthly, :decimal, required: true, desc: 'Car subscription price. Decimal value.'
-    param :model_id, :number, required: true, desc: 'Car model id. Model must exist.'
-    param :maker_id, :number, required: true, desc: 'Car maker id. Maker must exist.'
-    param :color_id, :number, required: true, desc: 'Car color id. Color must exist.'
-  end
-  error code: 400, desc: 'Bad Request.', meta: 'Review request endpoint.'
-  error code: 408, desc: 'Request Timeout.', meta: 'Contact support team.'
-  error code: 422, desc: 'Unprocessable Entity.', meta: 'Review request parameters.'
-  error code: 500, desc: 'Internal Server Error.', meta: 'Contact support team.'
+  param_group :car
+  param_group :car_generic_errors
   returns code: 201, desc: 'Resource Created.' do
     property :car, Hash, desc: 'Created Car Object' do
-      property :available_from, String, required: true, desc: 'Available from, when the car is available for booking.'
-      property :year, :number, required: true, desc: 'The year of the car.'
-      property :monthly, :decimal, required: true, desc: 'Car subscription price.'
-      property :model_id, :number, required: true, desc: 'Car model id.'
-      property :maker_id, :number, required: true, desc: 'Car maker id.'
-      property :color_id, :number, required: true, desc: 'Car color id.'
-      property :created_at, String, required: true, desc: 'When the car object was created.'
-      property :updated_at, String, required: true, desc: 'When the car object was updated.'
+      param_group :car_response
     end
   end
   def create
@@ -51,30 +77,12 @@ class CarsController < ApplicationController
   # PUT OR PATCH /cars/:id
   api :PUT, '/cars/:id', 'Endpoint to update a specific car. You can also use PATCH method.'
   formats ['json']
-  param :car, Hash, required: true, desc: 'Car params' do
-    param :available_from, String, required: true, desc: 'Available from, when the car is available for booking.
-                                                          Use the following date format YYYY-mm-dd (e.g 2020/11/14).'
-    param :year, :number, required: true, desc: 'The year of the car.'
-    param :monthly, :decimal, required: true, desc: 'Car subscription price. Decimal value.'
-    param :model_id, :number, required: true, desc: 'Car model id. Model must exist.'
-    param :maker_id, :number, required: true, desc: 'Car maker id. Maker must exist.'
-    param :color_id, :number, required: true, desc: 'Car color id. Color must exist.'
-  end
-  error code: 400, desc: 'Bad Request.', meta: 'Review request endpoint.'
+  param_group :car
+  param_group :car_generic_errors
   error code: 404, desc: 'Not Found.', meta: 'Review car identifier.'
-  error code: 408, desc: 'Request Timeout.', meta: 'Contact support team.'
-  error code: 422, desc: 'Unprocessable Entity.', meta: 'Review request parameters.'
-  error code: 500, desc: 'Internal Server Error.', meta: 'Contact support team.'
   returns code: 200, desc: 'Resource Updated.' do
     property :car, Hash, desc: 'Updated Car Object' do
-      property :available_from, String, required: true, desc: 'Available from, when the car is available for booking.'
-      property :year, :number, required: true, desc: 'The year of the car.'
-      property :monthly, :decimal, required: true, desc: 'Car subscription price.'
-      property :model_id, :number, required: true, desc: 'Car model id.'
-      property :maker_id, :number, required: true, desc: 'Car maker id.'
-      property :color_id, :number, required: true, desc: 'Car color id.'
-      property :created_at, String, required: true, desc: 'When the car object was created.'
-      property :updated_at, String, required: true, desc: 'When the car object was updated.'
+      param_group :car_response
     end
   end
   def update
--- backend/config/routes.rb
+++ backend/config/routes.rb
@@ -2,5 +2,5 @@ Rails.application.routes.draw do
   apipie
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
-  resources :cars, param: :id, only: %i[create update]
+  resources :cars, param: :id, only: %i[create update index]
 end
