0dfbca478d265fb7cae9a01a1dda8f8c9bcfea35
BernardoMG <bernardo.marquesg@gmail.com>
2020-11-16 00:32:08 +0000

    Fix migrations

--- backend/db/migrate/20201115200631_add_indexes_for_cars_table.rb
+++ backend/db/migrate/20201115200631_add_indexes_for_cars_table.rb
@@ -3,7 +3,5 @@ class AddIndexesForCarsTable < ActiveRecord::Migration[6.0]
     add_index(:cars, :monthly, using: 'btree')
     add_index(:cars, :year, using: 'btree')
     add_index(:cars, :available_from, using: 'btree')
-    add_index(:cars, :maker_id, using: 'btree')
-    add_index(:cars, :color_id, using: 'btree')
   end
 end
